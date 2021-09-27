250c5c9ead2e6c18331927d270de5a13a3bc2a72
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-09-27 22:41:06 +0200

    First commit

---
+++ src/Autocarro.java
@@ -0,0 +1,16 @@
+public class Autocarro {
+
+
+    public Autocarro() {
+
+        int carryingCapacity;
+        String loadType;
+        String destination;
+        String departure;
+        String departureDate;
+        String arrivalDate;
+
+    }
+
+
+}
