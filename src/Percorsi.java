6ea2255be6f602a0781e0810479f768eea21036d
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-10-08 16:07:25 +0200

    Fix Percorsi class. Added getters & setters methods, added Percorsi constructor in Trasporti.main.

--- src/Percorsi.java
+++ src/Percorsi.java
@@ -9,10 +9,25 @@ public class Percorsi {
     public Percorsi(String arrivo, String partenza, int dataPartenza, int dataArrivo) {
         destination = arrivo;
         departure = partenza;
-        dataPartenza = departureDate;
-        dataArrivo = arrivalDate;
+        departureDate =dataPartenza;
+        arrivalDate = dataArrivo;
     }
 
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public int getDepartureDate() {
+        return departureDate;
+    }
+
+    public int getArrivalDate() {
+        return arrivalDate;
+    }
 
     public String percorso(){
         return "Il percorso va da " + departure + " fino a " + destination + ".";
--- src/Trasporti.java
+++ src/Trasporti.java
@@ -4,6 +4,7 @@ public class Trasporti {
 
     public static void main(String[] args) {
         Autocarro autocarro = new Autocarro();
+        Percorsi percorso = new Percorsi("Trieste", "Milano", 01102021,04102021);
 
         String targa = autocarro.getLicensePlate();
 
